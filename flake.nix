2c7c63b22e692024cd87a8646b07bf04759ead1c
wi11-holdsworth <83637728+wi11-holdsworth@users.noreply.github.com>
2025-09-18 11:05:52 +1000

    rendercv flake init

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      devShells."${system}".default =
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        pkgs.mkShell {
+          packages = with pkgs; [
+            rendercv
+          ];
+        };
+    };
+}
